3fd2b11fd83b367e32f5adc522c1315930976d36
KevSalcedo <kevsalcedom@gmail.com>
2023-06-15 12:13:46 -0500

    Create CryptoCard widget.

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -133,3 +133,41 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 }
+
+class CryptoCard extends StatelessWidget {
+  const CryptoCard({
+    super.key,
+    required this.cryptoCurrency,
+    required this.foreignExchange,
+    required this.selectedCurrency,
+  });
+
+  final String cryptoCurrency;
+  final String? foreignExchange;
+  final String selectedCurrency;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $cryptoCurrency = $foreignExchange $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
