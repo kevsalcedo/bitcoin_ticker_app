720e69b44d2e5b667c4b25513e4f6ac63b54047f
KevSalcedo <kevsalcedom@gmail.com>
2023-06-15 12:14:07 -0500

    Use CryptoCard widget to pass foreignExchange: isWaiting ? '?' : coinValues['BTC'],

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -100,26 +100,20 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = $foreignExchange $selectedCurrency',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          CryptoCard(
+            cryptoCurrency: 'BTC',
+            foreignExchange: isWaiting ? '?' : coinValues['BTC'],
+            selectedCurrency: selectedCurrency,
+          ),
+          CryptoCard(
+            cryptoCurrency: 'ETH',
+            foreignExchange: isWaiting ? '?' : coinValues['ETH'],
+            selectedCurrency: selectedCurrency,
+          ),
+          CryptoCard(
+            cryptoCurrency: 'LTC',
+            foreignExchange: isWaiting ? '?' : coinValues['LTC'],
+            selectedCurrency: selectedCurrency,
           ),
           Container(
             height: 150.0,
